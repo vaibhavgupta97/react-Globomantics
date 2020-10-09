f77e48f9b18af6db1bbfa3ed41e1e1bf5983d59f
Vaibhav Gupta <vaibhav16feb1997@gmail.com>
2020-10-09 08:38:12 +0530

    9oct08:38

---
+++ src/main-page/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const  Header=() =>(
+   
+<header className="row">
+        <div className="col-md-5">
+            <img src=" " alt=""/>
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+        Providing houses world wide
+        </div>
+    </header>
+);
+
+export default Header
